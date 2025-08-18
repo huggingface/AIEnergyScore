6e51e316ec4e87d73c7b5c38d77e58c9d8942acc
Sasha Luccioni <sasha.luccioni@huggingface.co>
2025-08-18 10:43:45 -0400

    Update entrypoint.sh
    Updating the config_dir to the right one (I think!)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 python /check_h100.py
 if [[ $? = 0 ]]; then
-    optimum-benchmark --config-dir /optimum-benchmark/examples/energy_star/ $@
+    optimum-benchmark --config-dir /optimum-benchmark/energy_star/ $@
 fi
